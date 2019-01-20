c4f0e313b54844d70256d9bd287d66c255397e17
cliffdawg <cliffordyin@gmail.com>
2019-01-19 20:32:22 -0800

    Try to fix date creation

--- database/index.js
+++ database/index.js
@@ -149,7 +149,7 @@ async function createGoal(create, callback) {
       console.log(`Creating: INSERT INTO goals (first_var, second_var, third_var) 
 		VALUES ( \'${create.firstVar}\', 'Dummy username', ${create.thirdVar});`);
 	  connection.query(`INSERT INTO goals (goal, username, timeStamp) 
-		VALUES (?, ?, ?);`, [create.goal, 'Dummy username', Date()], (err, rows, fields) => {
+		VALUES (?, ?, ?);`, [create.goal, 'Dummy username', new Date()], (err, rows, fields) => {
 		  connection.release();
 		  if (err) {
 			console.log(`Failure: ${err}`);
