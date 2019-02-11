443db257b4f6c9a873f890e1af3764a0dac8ab71
cliffdawg <cliffordyin@gmail.com>
2019-02-10 16:25:33 -0800

    Added username data to posted goal object

--- database/index.js
+++ database/index.js
@@ -147,9 +147,9 @@ async function createGoal(create, callback) {
 	  } 
 	  console.log('Connected!');
       console.log(`Creating: INSERT INTO goals (first_var, second_var, third_var) 
-		VALUES ( \'${create.goal}\', 'Dummy username', ${create.thirdVar});`);
+		VALUES ( \'${create.goal}\', ${create.username}...`);
 	  connection.query(`INSERT INTO goals (goal, username, timeStamp) 
-		VALUES (?, ?, ?);`, [create.goal, 'Dummy username', new Date()], (err, rows, fields) => {
+		VALUES (?, ?, ?);`, [create.goal, create.username, new Date()], (err, rows, fields) => {
 		  connection.release();
 		  if (err) {
 			console.log(`Failure: ${err}`);
